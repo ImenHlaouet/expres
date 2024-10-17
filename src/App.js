f79b1799a48dec9f3028fb38c81f03c386fe0b1f
Imen <Imen@gmail.com>
2024-10-17 11:07:10 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ab  from './Image/a.jpg'
+import Hello from './componet/Hello';
 function App() {
+  
+
+  const name='jhon';
+  const a=1;
+  const b=5;
+  let firstName='Imen';
+  let secondName='Hlaouet';
+  const multuplication = (x,y)=>x*y;
+  let btn='Ajouter';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello/>
+      <h1 style={{ color: "red", fontSize: 60, width:50, justifyContent: 'flex-start'}}>Hello Imen</h1>
+      <img src={ab} width={200} height={400}/>
+      <h2>{name} </h2>
+      <h3>{a+b}</h3>
+      <h3 >{firstName + ' ' + secondName}</h3>
+      <h4>{multuplication(5,7)}</h4>
+      <button>{btn}</button>
+
     </div>
   );
 }
---
+++ src/componet/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div>
+      <h1>fwfejjforkfprkfkrfkrfnrkfjrk</h1>
+    </div>
+  )
+}
+
+export default Hello
+
